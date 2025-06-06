f0685f3817ad6a9d4826787e5719d4410156132c
weibalife <weibalife520@gmail.com>
2025-06-06 11:08:39 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+USER_NAME = "tailway"         # 用户名
+UUID = "84fe6a1e-966c-49e0-8f8c-23063f402d9f"                     # UUID，留空则自动生成
+PORT = 43399                   # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiZDEyMTE5N2M3NmE4YjU5MzI4OGFhYjgwYTgxYTc1NzYiLCJ0IjoiN2RkNTg1NzYtNzMwYi00ODI2LWI1NzctNzI0NzM1Y2Y0Zjc0IiwicyI6Ik9UUTBNV1ppTXpBdFl6bGxZeTAwWmpNM0xUaGlPR1V0TkRrMlpXTTNaVEF3WkRVNSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
